7ef6f2b8e9ecd05df95bc7b47ecbd0380a1f53cf
akash-1316 <freefireakash73@gmail.com>
2025-09-16 21:23:52 +0530

    Update login.jsx

--- ADMIN/src/components/Login/login.jsx
+++ ADMIN/src/components/Login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import axios from "axios"; // Add this import
-import { StoreContext } from "../../context/StoreContext";
+import { StoreContext } from "../../Context/StoreContext";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
