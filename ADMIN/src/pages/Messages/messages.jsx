00061ce73e7e4b5813791ea72a50d3504fac72bc
akash-1316 <freefireakash73@gmail.com>
2025-09-16 21:15:57 +0530

    Update messages.jsx

--- ADMIN/src/pages/Messages/messages.jsx
+++ ADMIN/src/pages/Messages/messages.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { StoreContext } from "../../context/StoreContext";
+import { StoreContext } from "../../context/StoreContext.jsx";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import "./Messages.css";
